19d28980ab1549192d3c04dd8662e16d11bb48bd
Altin Hysa <altinhysa84@gmail.com>
2025-11-27 23:13:33 +0100

    test: flydev

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: true,
-    port: 3001,
+    port: 4000,
     strictPort: true,
     allowedHosts: ["localhost", "127.0.0.1", ".fly.dev"],
     base: '/agent'
