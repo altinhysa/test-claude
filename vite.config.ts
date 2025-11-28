d33002cf855617f33e1d98bf44c6d4fdc77fa9ae
Altin Hysa <altinhysa84@gmail.com>
2025-11-28 13:44:44 +0100

    test: flydev

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: true,
-    port: 3001,
+    host: '0.0.0.0',
+    port: 3000,
     strictPort: true,
     allowedHosts: ["localhost", "127.0.0.1", ".fly.dev", "bolt-clone-agent.fly.dev"],
   },
+  base: '/agent/', // VERY IMPORTANT
+
   plugins: [react()],
 })
